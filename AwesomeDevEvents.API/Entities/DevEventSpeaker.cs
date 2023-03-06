6ee63ad2cc764d6a6b2b5ca58c292bb24c089027
Victor Oliveira <vittoliveiraa@live.com>
2023-03-06 09:51:14 -0300

    Aula 1 - Criação da WebAPI e Controllers

---
+++ AwesomeDevEvents.API/Entities/DevEventSpeaker.cs
@@ -0,0 +1,10 @@
+﻿namespace AwesomeDevEvents.API.Entities
+{
+    public class DevEventSpeaker
+    {
+        public Guid Id { get; set; }
+        public string TalkName { get; set; }
+        public string TalkDescription { get; set; }
+        public string LinkedInProfile { get; set; }
+    }
+}
